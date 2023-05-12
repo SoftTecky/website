00d9271c42d1c1e5c1ffc954f602d57e2414da2a
Jordanius <jordaniusbusiness@gmail.com>
2023-05-11 18:18:24 -0600

    11/0512023

--- src/views/LoginScreen.vue
+++ src/views/LoginScreen.vue
@@ -59,25 +59,18 @@
 export default {};
 </script>
 
-<style scoped>
+<style>
 .text-muted {
   color: #6c757d !important;
 }
-.form-label {
-  color: #37759c;
-}
-.gradient-custom {
-  /* fallback for old browsers */
-  background: #37759c;
 
-  /* Chrome 10-25, Safari 5.1-6 */
+.gradient-custom {
+  background: #37759c;
   background: -webkit-linear-gradient(
     to right,
     rgba(106, 17, 203, 1),
     rgba(37, 117, 252, 1)
   );
-
-  /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
   background: linear-gradient(
     to right,
     rgba(106, 17, 203, 1),
