bbc2b36241f8e465f0b87a3f5574f9fe9acb3e3a
Antonio Reyna Espinoza <antoniore102001@gmail.com>
2023-05-22 01:04:29 -0600

    Add initial notification view

---
+++ src/components/NotificationItem.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { defineProps } from "vue";
+
+defineProps(["description", "datetime", "user"]);
+</script>
+
+<template>
+  <div class="notification--item">
+    <img
+      src="https://this-person-does-not-exist.com/img/avatar-gen29316a9cb9e0c716cbe841f496d91950.jpg"
+    />
+    <div>
+      <p>
+        <strong>{{ user }}: </strong> {{ description }}
+      </p>
+      <p>{{ datetime }}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+/* create a style for the class notification--item with an image to the left and a description to the right with the date below */
+
+.notification--item {
+  display: flex;
+  align-items: center;
+  margin-bottom: 0.5rem;
+  background-color: white;
+  padding: 1rem;
+  border-radius: 0.5rem;
+}
+
+.notification--item img {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  margin-right: 1rem;
+}
+
+.notification--item div {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+
+.notification--item div p {
+  margin: 0;
+}
+</style>
---
+++ src/views/NotificationsView.vue
@@ -0,0 +1,57 @@
+<script setup>
+import { ref } from "vue";
+import NotificationItem from "../components/NotificationItem";
+
+const notifications = ref([
+  {
+    id: 1,
+    text: "Notification text",
+    datetime: "2021-01-01 00:00:00",
+    user: "User name",
+  },
+  {
+    id: 2,
+    text: "Notification text",
+    datetime: "2021-01-01 00:00:00",
+    user: "User name",
+  },
+  {
+    id: 3,
+    text: "Notification text",
+    datetime: "2021-01-01 00:00:00",
+    user: "User name",
+  },
+]);
+</script>
+
+<template>
+  <div class="container">
+    <div class="notification--container">
+      <NotificationItem
+        v-for="n in notifications"
+        :key="n.id"
+        :description="n.text"
+        :user="n.user"
+        :datetime="n.datetime"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  padding: 1rem 0;
+}
+
+.notification--container {
+  background-color: #A8D6EC;
+  padding: 1rem;
+  border-radius: 0.5rem;
+  width: 60%;
+  height: 100%;
+}
+</style>
